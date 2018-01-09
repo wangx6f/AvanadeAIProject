1ab34358000703ee42d9afb5a147b3cf5c34b214
xinyuanwang <wang.x.6f@gmail.com>
2018-01-08 18:39:42 -0800

    add extension for UIView

---
+++ AvanadeAIProject/UIView+IBInspectable.swift
@@ -0,0 +1,77 @@
+//
+//  UIView+IBInspectable.swift
+//  AvanadeAIProject
+//
+//  Created by Xinyuan Wang on 1/8/18.
+//  Copyright © 2018 Avanade. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    @IBInspectable var shadowOpacity : Float  {
+        set {
+            self.layer.shadowOpacity = newValue
+        }
+        get {
+            return self.layer.shadowOpacity
+        }
+    }
+    
+    @IBInspectable var shadowColor : UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.shadowColor!)
+        }
+        set {
+            self.layer.shadowColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: Double {
+        get {
+            return Double(self.layer.shadowRadius)
+        }
+        set {
+            self.layer.shadowRadius = CGFloat(newValue)
+        }
+    }
+    
+    @IBInspectable var shadowOffset: CGSize {
+        get {
+            return self.layer.shadowOffset
+        }
+        set {
+            self.layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable var borderWidth: Double {
+        get {
+            return Double(self.layer.borderWidth)
+        }
+        set {
+            self.layer.borderWidth = CGFloat(newValue)
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        get {
+            return UIColor(cgColor: self.layer.borderColor!)
+        }
+        set {
+            self.layer.borderColor = newValue?.cgColor
+        }
+    }
+    
+    @IBInspectable var cornerRadius: Double {
+        get {
+            return Double(self.layer.cornerRadius)
+        }
+        set {
+            self.layer.cornerRadius = CGFloat(newValue)
+        }
+    }
+}
+
