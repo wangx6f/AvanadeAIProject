9449874423b1645a42c2f94fe70ad037b4150152
jasonkim233233 <jasonkim233233@gmail.com>
2018-01-15 00:08:51 -0800

    Add files via upload

---
+++ TableCell.swift
@@ -0,0 +1,27 @@
+//
+//  TableCell.swift
+//  AvanadeAIProject
+//
+//  Created by Jason Kim on 1/14/18.
+//  Copyright © 2018 Avanade. All rights reserved.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+
+    @IBOutlet var imagesView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
