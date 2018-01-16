e4092460a2fbfc4465257404bcaf38bd95de1c42
xinyuanwang <wang.x.6f@gmail.com>
2018-01-15 23:38:54 -0800

    Revert "Merge branch 'model'"
    This reverts commit 8a6fbe50128a6d450a39a417873fcb974eca9036, reversing
    changes made to 2c23a246bfaf129cd6c5036d4ef1bed422730730.

--- AvanadeAIProject/Controllers/Detail/CommentDetailViewController.swift
+++ AvanadeAIProject/Controllers/Detail/CommentDetailViewController.swift
@@ -10,22 +10,10 @@ import UIKit
 
 class CommentDetailViewController: UIViewController {
 
-<<<<<<< HEAD:AvanadeAIProject/Controllers/Detail/CommentDetailViewController.swift
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-=======
-    @IBOutlet weak var imageView: UIImageView!
-    
-    var artwork: Artwork!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        artwork = DataManager.sharedInstance.artworkWith(id: 1)
-        updateUI()
->>>>>>> model:AvanadeAIProject/DetailViewController.swift
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,11 +23,5 @@ class CommentDetailViewController: UIViewController {
 
     @IBAction func onBackPressed(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
-    }
-    
-    private func updateUI() {
-        self.title = artwork.title
-        imageView.image = artwork.afterImage
-        // etc for all the UI components on the page
     }
 }
