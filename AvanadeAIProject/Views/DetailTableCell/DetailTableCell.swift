aeeb3a862bbccf181ec76f5a02c32dd63af5d08b
Jason Kim <jasonkim233233@gmail.com>
2018-05-09 22:37:26 -0700

    Reference Image containing Artist's original artwork now displayed in 2nd tab.

--- AvanadeAIProject/Views/DetailTableCell/DetailTableCell.swift
+++ AvanadeAIProject/Views/DetailTableCell/DetailTableCell.swift
@@ -49,9 +49,10 @@ class DetailTableCell: UITableViewCell {
             loadImageAsync(url: artwork.afterImageURL!) { (image) in
                 self.artworkImageView.loadImage(after: image)
             }
-            /*loadImageAsync(url: artwork.artistImageURL!) { (image) in
+            loadImageAsync(url: artwork.referenceImageURL!) { (image) in
                 self.artworkImageView.loadImage(artist: image)
-            }*/
+            
+            }
             refreshArtwork(artwork)
             
         }
